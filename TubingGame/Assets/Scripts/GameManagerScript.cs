b8602dbf6ab3ca5f28ba481ee786e04b6a3786b4
Scott Aquino <scott.aquino@mymail.champlain.edu>
2018-03-23 14:21:52 -0400

    Added player 2 capability and collision

---
+++ TubingGame/Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour {
+
+	int playerCount;
+	public GameObject player2;
+
+
+	// Use this for initialization
+	void Start () {
+		playerCount = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.RightBracket) && playerCount < 1)
+		{
+			playerCount++;
+		}
+		if (Input.GetKey (KeyCode.LeftBracket) && playerCount > 0) 
+		{
+			playerCount--;
+		}
+		if(Input.GetKey(KeyCode.KeypadEnter))
+		{
+			if(playerCount == 1 && player2)
+			{
+				player2.SetActive (true);
+			}
+		}
+	}
+
+
+}
