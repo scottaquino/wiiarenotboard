7b63bca208e0b7d3ca171d43b1956bff624d236d
quincy-buchanan <35669413+quincy-buchanan@users.noreply.github.com>
2018-03-24 16:54:28 -0400

    Camera and River
    -Camera follows lead player
    -Start on procedural generation of river

---
+++ TubingGame/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "Player") {
+			Vector3 newPos = new Vector3 (0, col.gameObject.transform.position.y + 4.25f, -10.0f);
+			transform.position = newPos;
+		}
+	}
+}
---
+++ TubingGame/Assets/Scripts/SpawnRiver.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnRiver : MonoBehaviour {
+
+	public GameObject riverBank;
+	Vector3 newPos;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ("Spawn");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator Spawn()
+	{
+		yield return new WaitForSeconds (1.0f);
+		while (true) {
+			newPos = new Vector3 (Random.Range (-3.0f, 3.0f), transform.position.y, transform.position.z);
+			Instantiate(riverBank, newPos, Quaternion.identity);
+			yield return new WaitForSeconds (.5f);
+		}
+	}
+}
