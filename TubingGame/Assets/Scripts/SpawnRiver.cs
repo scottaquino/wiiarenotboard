545ac1524e29cf575ba08d9748c9b67082591944
quincy-buchanan <35669413+quincy-buchanan@users.noreply.github.com>
2018-04-07 14:15:14 -0400

    Minor QoL

--- TubingGame/Assets/Scripts/SpawnRiver.cs
+++ TubingGame/Assets/Scripts/SpawnRiver.cs
@@ -32,9 +32,9 @@ public class SpawnRiver : MonoBehaviour {
 		if (col.gameObject.tag == "Player" && !spawned) {
 			newChunk = riverBank [Random.Range(0, numChunks)];
 			if(newChunk == riverBank[0])
-				newPos = new Vector3 (transform.position.x + 13.15f, transform.position.y - (newChunk.GetComponent<SpriteRenderer> ().bounds.size.y / 2) - .3f, -1);
+				newPos = new Vector3 (transform.position.x + 13.15f, transform.position.y - (newChunk.GetComponent<SpriteRenderer> ().bounds.size.y / 2) - .2f, -1);
 			else if(newChunk == riverBank[1])
-				newPos = new Vector3 (transform.position.x - 10.0f, transform.position.y - (newChunk.GetComponent<SpriteRenderer> ().bounds.size.y / 2) - .3f, -1);
+				newPos = new Vector3 (transform.position.x - 10.0f, transform.position.y - (newChunk.GetComponent<SpriteRenderer> ().bounds.size.y / 2) - .2f, -1);
 			Instantiate (newChunk, newPos, Quaternion.identity);
 			GetComponent<SpawnRiver> ().enabled = false;
 			spawned = true;
