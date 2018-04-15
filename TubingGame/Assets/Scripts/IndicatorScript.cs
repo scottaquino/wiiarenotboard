720fd4c5e4ae3bfbf8ab292d0caff56c06936f41
Andersen Pinckney <andersen.pinckney@mymail.champlain.edu>
2018-04-14 20:22:57 -0400

    Adding Indicators
    Indicator works for rocks, doesn't blink yet.

---
+++ TubingGame/Assets/Scripts/IndicatorScript.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndicatorScript : MonoBehaviour
+{
+	//Locate GameObjects
+	public GameObject target;
+	public GameObject minDistance;
+	public GameObject cam;
+
+	//Locations
+	public float initialIndicatorOffset;
+	private Vector2 curPos;
+	private bool coroutineStarted = false;
+
+	//Blinking
+	public float blinkSpeed;
+
+	private void Start()
+	{
+		target = FindParentWithTag(this.gameObject, "Obstacle");
+		minDistance = FindMinDistance(this.gameObject, "Min_Distance");
+		cam = GameObject.Find("MainCamera");
+		StartPosition();
+	}
+
+	//Returns Obstacle
+	public GameObject FindParentWithTag(GameObject indicator, string tag)
+	{
+		Transform t = indicator.transform;
+		while (t.parent != null)
+		{
+			if (t.parent.tag == tag)
+			{
+				return t.parent.gameObject;
+			}
+			t = t.parent.transform;
+		}
+		return null; //Could not find parent with tag
+		//Debug.Log("Cannot find object with tag");
+	}
+
+	//Returns minimum distance indicator can be from obstacle
+	public GameObject FindMinDistance(GameObject indicator, string minD)
+	{
+		Transform t = indicator.transform.parent;
+		GameObject minDistance = t.Find(minD).gameObject;
+		return minDistance;
+	}
+
+	//Transform start position of indicator to X of obstacle and Y + 50 of camera
+	public void StartPosition()
+	{
+		Vector2 thisPos = new Vector2();
+		thisPos.x = target.transform.position.x; //Get obstacle's X
+		thisPos.y = cam.transform.position.y + initialIndicatorOffset; //Get beneath camera
+
+		//Set indicator's position
+		this.transform.position = thisPos;
+		curPos = thisPos;
+	}
+
+	private void Update()
+	{
+		SetPosition();
+		Blinking();
+	}
+
+	private void SetPosition()
+	{
+		curPos = this.transform.position;
+		if (curPos.y <= minDistance.transform.position.y)
+		{
+			if (!coroutineStarted)
+			{
+				coroutineStarted = true;
+				StartCoroutine("DestroyIndicator");
+				Debug.Log("IT HAS STARTED");
+			}
+
+		}
+		else if (curPos.y >= cam.transform.position.y - 4.5f && curPos.y > minDistance.transform.position.y)
+		{
+			Vector2 newPos = new Vector2();
+			newPos.x = this.transform.position.x;
+			newPos.y = cam.transform.position.y - 4.5f;
+
+			this.transform.position = newPos;
+			curPos = newPos;
+		}
+	}
+
+	private void Blinking()
+	{
+		//null
+	}
+
+	IEnumerator DestroyIndicator()
+	{
+		yield return new WaitForSeconds(1.5f);
+		Destroy(this.gameObject);
+	}
+}
---
+++ TubingGame/Assets/Scripts/IndicatorTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndicatorTrigger : MonoBehaviour
+{
+	public GameObject indicator;
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.tag == "Obstacle_Trigger")
+		{
+			GameObject colParent = collision.transform.parent.gameObject;
+			GameObject childObject = Instantiate(indicator) as GameObject;
+			childObject.transform.parent = colParent.transform;
+		}
+	}
+}
