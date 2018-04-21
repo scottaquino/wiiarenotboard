9bde72c055cae1aab2042fd837292fe09d881701
quincy-buchanan <35669413+quincy-buchanan@users.noreply.github.com>
2018-04-21 16:40:05 -0400

    Whirlpools Work
    Somehow...

--- TubingGame/Assets/Scripts/WhirlpoolScript.cs
+++ TubingGame/Assets/Scripts/WhirlpoolScript.cs
@@ -6,11 +6,25 @@ public class WhirlpoolScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "Player") {
+			StartCoroutine ("PushAway");
+		}
+	}
+
+	private IEnumerator PushAway()
+	{
+		yield return new WaitForSeconds (.5f);
+		GetComponent<PointEffector2D> ().forceMagnitude = 150.0f;
+		yield return new WaitForSeconds (2.0f);
+		GetComponent<PointEffector2D> ().forceMagnitude = -25.0f;
 	}
 }
