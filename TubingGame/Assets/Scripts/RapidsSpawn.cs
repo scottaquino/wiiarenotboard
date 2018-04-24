487aea418e0d7f07820b7760f036486ce778072d
quincy-buchanan <35669413+quincy-buchanan@users.noreply.github.com>
2018-04-24 10:14:29 -0400

    Rapids Again

--- TubingGame/Assets/Scripts/RapidsSpawn.cs
+++ TubingGame/Assets/Scripts/RapidsSpawn.cs
@@ -7,7 +7,7 @@ public class RapidsSpawn : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		int temp = Random.Range (0, 100);
-		if (temp <= 50) {
+		if (temp <= 40) {
 			GetComponent<SpriteRenderer> ().enabled = false;
 			GetComponent<PolygonCollider2D> ().enabled = false;
 		}
