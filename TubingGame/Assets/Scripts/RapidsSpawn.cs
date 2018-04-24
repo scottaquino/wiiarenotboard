3ddad80c1c34a37c1735b927d7a1b7bc7bf85676
quincy-buchanan <35669413+quincy-buchanan@users.noreply.github.com>
2018-04-24 10:11:07 -0400

    Rapids

---
+++ TubingGame/Assets/Scripts/RapidsSpawn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RapidsSpawn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int temp = Random.Range (0, 100);
+		if (temp <= 50) {
+			GetComponent<SpriteRenderer> ().enabled = false;
+			GetComponent<PolygonCollider2D> ().enabled = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
